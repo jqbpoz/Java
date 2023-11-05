78ae196833ebbff1c6414992d2060d854ad2c39a
Jakub Laptop <jqbpoz02@gmail.com>
2023-11-05 22:24:48 +0100

    Template

---
+++ Zadanie 4/src/Indiana.java
@@ -0,0 +1,17 @@
+public class Indiana implements Explorer{
+
+    @Override
+    public void underwaterMovesAllowed(int moves) {
+
+    }
+
+    @Override
+    public void setPlayerController(PlayerController controller) {
+
+    }
+
+    @Override
+    public void findExit() {
+
+    }
+}
