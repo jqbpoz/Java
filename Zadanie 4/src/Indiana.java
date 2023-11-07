41b123be4899c4932a3a65c0471f0a819f9f94e4
Jakub Laptop <jqbpoz02@gmail.com>
2023-11-07 17:00:59 +0100

    BlindPositionImplemented

--- Zadanie 4/src/Indiana.java
+++ Zadanie 4/src/Indiana.java
@@ -1,11 +1,56 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Queue;
+import java.util.LinkedList;
+
+//Należy dbać zby blind pozycja wykonywała to co pozycja kontrolera
 public class Indiana implements Explorer {
     int moves;
     PlayerController controller;
 
-    Direction directionNorth = Direction.NORTH;
-    Direction directionEast = Direction.EAST;
-    Direction directionSouth = Direction.SOUTH;
-    Direction directionWest = Direction.WEST;
+
+    public Direction opositeTo(Direction direction) {
+        switch (direction) {
+            case NORTH -> {
+                return Direction.SOUTH;
+            }
+            case EAST -> {
+                return Direction.WEST;
+            }
+            case SOUTH -> {
+                return Direction.NORTH;
+            }
+            case WEST -> {
+                return Direction.EAST;
+            }
+        }
+        return direction;
+    }
+
+    public Direction toLeft(Direction direction) {
+        switch (direction) {
+            case NORTH -> {
+                return Direction.WEST;
+            }
+            case EAST -> {
+                return Direction.NORTH;
+            }
+            case SOUTH -> {
+                return Direction.EAST;
+            }
+            case WEST -> {
+                return Direction.SOUTH;
+            }
+        }
+        return direction;
+    }
+
+    Position blindPosition = new Position(0, 0);
+
+    Set<Position> floodedBlindSet = new HashSet<>();
+    Set<Position> FireAndWallBlindSet = new HashSet<>();
+    Set<Position> whitePathSet = new HashSet<>();
+
 
     @Override
     public void underwaterMovesAllowed(int moves) {
@@ -19,19 +64,40 @@ public class Indiana implements Explorer {
 
     @Override
     public void findExit() {
-        try {
-            controller.move(Direction.NORTH);
+        boolean loop = true;
+        //kierunkami sterować będziemy w zaleności od potrzeby
 
-        } catch (OnFire onFireException) {
-            System.out.println("Gracz jest w ogniu! " + onFireException.getMessage());
-        } catch (Flooded floodedException) {
-            System.out.println("Gracz jest zalany! " + floodedException.getMessage());
-        } catch (Wall wallException) {
-            System.out.println("Gracz natrafił na ścianę! " + wallException.getMessage());
-        } catch (Exit exitException) {
-            System.out.println("Gracz znalazł wyjście! " + exitException.getMessage());
-        } catch (Exception e) {
-            System.out.println("Inny wyjątek: " + e.getMessage());
+        Direction currentDirection = Direction.EAST;
+        Position futureBlindPosition = null;
+        while (loop) {
+            System.out.println("moja ślepa pozycja" + blindPosition);
+            System.out.println();
+            try {
+                futureBlindPosition = currentDirection.step(blindPosition);
+                if (FireAndWallBlindSet.contains(futureBlindPosition)) {
+                    //tutaj w takim przypadku zmienimy kierunek i spóbujemy jeszcze raz damy continue
+                    break; // tutaj nie wykonamy tego kroku bo napotkamy na ścianę
+                }
+                controller.move(currentDirection);
+            } catch (OnFire onFireException) {
+                currentDirection = opositeTo(currentDirection);
+                continue;
+            } catch (Flooded floodedException) {
+                continue;
+            } catch (Wall wallException) {
+                System.out.println("Gracz natrafił na ścianę! ");
+                FireAndWallBlindSet.add(futureBlindPosition);
+                System.out.println("Do zbioru ścian dodano:" + futureBlindPosition);
+                //nie zmieniamy
+                continue;
+            } catch (Exit exitException) {
+                System.out.println("Gracz znalazł wyjście! ");
+                loop = false;
+                continue;
+            }
+            blindPosition = futureBlindPosition;
+            whitePathSet.add(blindPosition);
+
         }
 
     }
--- Zadanie 4/src/Main.java
+++ Zadanie 4/src/Main.java
@@ -1,18 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-      Controller controller = new Controller();
-      Indiana newIndiana = new Indiana();
-      newIndiana.setPlayerController(controller);
-      newIndiana.underwaterMovesAllowed(5);
-      newIndiana.findExit();
-      Position currentPosition = new Position(2,4);
-      Direction directionNorth = Direction.NORTH;
-      Direction directionEast = Direction.EAST;
-      Direction directionSouth = Direction.SOUTH;
-      Direction directionWest = Direction.WEST;
-
+        Controller controller = new Controller();
+        controller.setAll();
+        Indiana newIndiana = new Indiana();
+        newIndiana.setPlayerController(controller);
+        newIndiana.underwaterMovesAllowed(5);
+        newIndiana.findExit();
+        Position currentPosition = new Position(2, 4);
+        Direction directionNorth = Direction.NORTH;
+        Direction directionEast = Direction.EAST;
+        Direction directionSouth = Direction.SOUTH;
+        Direction directionWest = Direction.WEST;
 
 
     }
