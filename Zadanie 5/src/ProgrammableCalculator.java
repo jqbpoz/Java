2abee8095099488643ce57d6d40d18a99c1aef18
JqbPoz <89699784+jqbpoz@users.noreply.github.com>
2023-11-13 19:12:13 +0100

    template

---
+++ Zadanie 5/src/ProgrammableCalculator.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+
+class ProgrammableCalculator implements ProgrammableCalculatorInterface{
+    @Override
+    public void programCodeReader(BufferedReader reader) {
+
+    }
+
+    @Override
+    public void setStdin(LineReader input) {
+
+    }
+
+    @Override
+    public void setStdout(LinePrinter output) {
+    }
+
+    @Override
+    public void run(int line) {
+
+    }
+}
