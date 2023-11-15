1ed42f29027e504a900bb31fe9555ebdadb54c3d
JqbPoz <89699784+jqbpoz@users.noreply.github.com>
2023-11-15 15:13:36 +0100

    editMain

--- Zadanie 5/src/Main.java
+++ Zadanie 5/src/Main.java
@@ -1,38 +1,69 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
- public static void main(String[] args) {
+    public static void main(String[] args) {
         try {
-            // Otwórz plik do odczytu
-            String filePath = "./src/in.txt";
-            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader1 = new BufferedReader(new FileReader("./src/in1.txt"));
+            BufferedReader reader2 = new BufferedReader(new FileReader("./src/in2.txt"));
+            BufferedReader reader3 = new BufferedReader(new FileReader("./src/in3.txt"));
+            BufferedReader reader4 = new BufferedReader(new FileReader("./src/in4.txt"));
+            BufferedReader reader5 = new BufferedReader(new FileReader("./src/in5.txt"));
+            ProgrammableCalculator calculator1 = new ProgrammableCalculator();
+            ProgrammableCalculator calculator2 = new ProgrammableCalculator();
+            ProgrammableCalculator calculator3 = new ProgrammableCalculator();
+            ProgrammableCalculator calculator4 = new ProgrammableCalculator();
+            ProgrammableCalculator calculator5 = new ProgrammableCalculator();
 
-            // Przekaz plik do BufferedReader
-            BufferedReader programCodeReader = new BufferedReader(fileReader);
-
-            // Tworzymy instancję kalkulatora
-            ProgrammableCalculator calculator = new ProgrammableCalculator();
-
-            // Konfigurujemy obiekty LineReader i LinePrinter
             ProgrammableCalculatorInterface.LineReader lineReader = () -> {
-                // Implementuj odpowiednią logikę odczytu z konsoli lub innych źródeł
-                return null;
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Podaj Int");
+                String userInput = scanner.nextLine();
+                scanner.close();
+                return userInput;
             };
-
             ProgrammableCalculatorInterface.LinePrinter linePrinter = System.out::println;
+            calculator1.setStdout(linePrinter);
+            calculator1.setStdin(lineReader);
+            calculator2.setStdout(linePrinter);
+            calculator2.setStdin(lineReader);
+            calculator3.setStdout(linePrinter);
+            calculator3.setStdin(lineReader);
+            calculator4.setStdout(linePrinter);
+            calculator4.setStdin(lineReader);
+            calculator5.setStdout(linePrinter);
+            calculator5.setStdin(lineReader);
 
-            // Ustawiamy obiekty LineReader i LinePrinter w kalkulatorze
-            calculator.programCodeReader(programCodeReader);
-            calculator.setStdin(lineReader);
-            calculator.setStdout(linePrinter);
+            System.out.println("Program NR1 Sprawdzenie operacji");
+            calculator1.programCodeReader(reader1);
+            calculator1.run(1);
+            reader1.close();
+            System.out.println("\nexpected:\n6\n12\n3\n4\n8\nSTRING\n");
 
-            // Uruchamiamy program od linii 1
-            calculator.run(1);
+            System.out.println("Program NR2 Inna numeracja linii");
+            calculator2.programCodeReader(reader2);
+            calculator2.run(1);
+            reader2.close();
+            System.out.println("\nexpected:\n6\n12\n3\n4\n8\nSTRING\n");
 
-            // Zamykamy BufferedReader po zakończeniu pracy
-            programCodeReader.close();
+            System.out.println("Program NR3 Goto bez IF");
+            calculator3.programCodeReader(reader3);
+            calculator3.run(1);
+            reader3.close();
+            System.out.println("\nexpected:\nstart\nlinia 11 skok do 30\nskoczyłem z 12 do 30\n");
+
+            System.out.println("Program NR4 Test Goto z IF");
+            calculator4.programCodeReader(reader4);
+            calculator4.run(1);
+            reader4.close();
+            System.out.println("\nexpected:\na < b\nc > d\ne = f\na nie jest < od b\nc nie jest > od d\ne !=f\n");
+
+            System.out.println("Program NR5");
+            calculator5.programCodeReader(reader5);
+            calculator5.run(1);
+            reader5.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
